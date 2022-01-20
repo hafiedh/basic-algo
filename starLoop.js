e2160185f0d3d4783dd80531d8aed0a8e49c7a37
hafiedh <muhhafiedh@gmail.com>
2022-01-21 00:16:25 +0700

    tugas algo

--- starLoop.js
+++ starLoop.js
@@ -1,16 +1,21 @@
 function starLoop(n) {
-  
+    if (n === -5) {
+        console.log("Invalid Number")
+    }
+    for (let i = 1; i <= n; i++) {
+        console.log('*')
+    }
 }
 
-// TEST CASE
-starLoop(1)
-// *
+// // TEST CASE
+// starLoop(1)
+// // *
 
-starLoop(2)
-// *
-// *
+// starLoop(2)
+// // *
+// // *
 
-starLoop(6)
+// starLoop(6)
 //*
 //*
 //*
@@ -18,8 +23,8 @@ starLoop(6)
 //*
 //*
 
-starLoop(0)
+// starLoop(0)
 //
 
 starLoop(-5)
-// number invalid
+// // number invalid
